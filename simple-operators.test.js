d02cf0efbb76463424e8d6598f330ecb5efb03d5
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-01-22 16:49:55 +0000

    should return 0 for empty string

--- simple-operators.test.js
+++ simple-operators.test.js
@@ -1,8 +1,16 @@
+// http://osherove.com/tdd-kata-1/
+// yarn test in JestTest
+
 function addStuffUp(arg1) {
-  return arg1;
+  return arg1 === undefined ? 0 : arg1;
 };
 
 it('should allow arguments', () => {
   const addStuffUpFunc = addStuffUp(123);
-  expect(addStuffUpFunc).toEqual(123);
+  expect(addStuffUpFunc).toBe(123);
+});
+
+it('for an empty string it should return 0', () => {
+  const addStuffUpFunc = addStuffUp();
+  expect(addStuffUpFunc).toBe(0);
 });
