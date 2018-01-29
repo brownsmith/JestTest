8dc6a2e27b9acfb733b455c148f3ae4f7045b114
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-01-29 16:11:31 +0000

    moar refactoring

--- simple-operators.test.js
+++ simple-operators.test.js
@@ -1,33 +1,21 @@
 function addStuffUp(...args) {
 
   var argumentArray = [];
-
-  argumentArray.push(
-    arguments[0] ? arguments[0] : 0,
-    arguments[1] ? arguments[1] : 0,
-    arguments[2] ? arguments[2] : 0
-  );
-
+  argumentArray.push(arguments[0], arguments[1], arguments[2]);
+  
   var total = 0;
 
   for (arg in argumentArray) {
-    num = parseFloat(argumentArray[arg]);
+    var num = parseFloat(argumentArray[arg]);
+    num = (!num || num > 1000) ? 0 : num;
     if (num < 0) {
       throw 'negatives not allowed ' + num;
-    }
-    if (num > 1000) {
-      num = 0;
     }
     total += num;
   }
 
   return total;
 };
-
-it('should return 0 if no argument passed in', () => {
-  const addStuffUpFunc = addStuffUp();
-  expect(addStuffUpFunc).toEqual(0);
-});
 
 it('should allow arguments', () => {
   const addStuffUpFunc = addStuffUp(123);
@@ -61,6 +49,6 @@ it('negative numbers will throw an exception \'negatives not allowed\' and the n
 });
 
 it('should ignore a number greater than 1000, so 2 + 1002 = 2', () => {
-  const addStuffUpFunc = addStuffUp(2, 1002);
-  expect(addStuffUpFunc).toEqual(2);
+  const addStuffUpFunc = addStuffUp(1, 1002, 3);
+  expect(addStuffUpFunc).toEqual(4);
 });
